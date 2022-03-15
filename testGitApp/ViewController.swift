900301b66f8fb7f8b902734283becd2357df9f31
Atsushi1216 <ats1216@gmail.com>
2022-03-15 19:10:28 +0900

    Initial Commit

---
+++ testGitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGitApp
+//
+//  Created by 菅原 惇 on 2022/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
